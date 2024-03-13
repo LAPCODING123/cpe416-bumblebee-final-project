56ad9803f7e00635df16a4611a2ba8343f7ca25f
Chris Lawson <git@chrislawson.dev>
2024-03-13 10:39:24 -0700

    feat: cm conversion

--- line-follow/src/distance_sensor.cpp
+++ line-follow/src/distance_sensor.cpp
@@ -7,7 +7,7 @@ u08 get_dist_sensor_raw()
     return analog(DIST_SENS_AN_PIN);
 }
 
-// float get_dist_sensor_cm()
-// // {
-// //     return get_dist_sensor_raw()/CONVERSION_FACTOR;
-// // }
+float get_dist_sensor_cm()
+{
+    return (6762 / (get_dist_sensor_raw() - 9)) - 4;
+}
